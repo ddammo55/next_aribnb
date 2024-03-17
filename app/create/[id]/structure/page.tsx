0adaa6db0833b91515c6f7b2603897cb2a052ca1
Won ho Choi <wonhochoi@Wonui-Macmini.local>
2024-03-18 05:56:43 +0900

    category

---
+++ app/create/[id]/structure/page.tsx
@@ -0,0 +1,21 @@
+
+import { SelctetCategory } from "@/app/components/SelectedCategory";
+
+export default function StrucutreRoute({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          Which of these best describe your Home?
+        </h2>
+      </div>
+
+      <form >
+        <input type="hidden" name="homeId" value={params.id} />
+        <SelctetCategory />
+
+        
+      </form>
+    </>
+  );
+}
